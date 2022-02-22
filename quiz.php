96bb7a4aa838bf00813e3943c0f6c0957acc7db5
Sanjeet Mistry <sanjeetm@axioned.com>
2022-02-22 17:01:17 +0530

    Add User-name and Logout button in Header

--- quiz.php
+++ quiz.php
@@ -6,15 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Quiz</title>
     <link rel="stylesheet" href="css/override.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/quiz.css">
 </head>
 <body>
+    <?php
+    require('utility/base-url.php');
+    if(isset($_GET["logout"])) {
+        setcookie(EMAIL, "", time() - 300, "/", "", 0);
+        header('Location: ' . URL . '/login.php');
+    }
+    ?>
     <header>
         <div class="wrapper">
             <h1>
                 <a href="#" title="Online Quiz">Online Quiz</a>
             </h1>
+            <div class="control-buttons">
+                <span title="Logout" class="user-email"><?php echo isset($_COOKIE[EMAIL]) ? "Welcome! " . $_COOKIE[EMAIL] : ""; ?></span>
+                <a href='<?php echo constant('URL').'/home.php?logout=true'?>' title="Logout">Logout</a>
+            </div>
         </div>
     </header>
     <div class="wrapper">
